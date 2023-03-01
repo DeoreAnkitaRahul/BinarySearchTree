fea6bdc3fd7910c0a05031107fda87ef02a0e337
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-03-01 16:31:13 +0530

    Create BST N Adding 30,56 N 70

--- Program.cs
+++ Program.cs
@@ -5,6 +5,13 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Binary Search Tree Program");
+            BinarySearchTree<int> bst = new BinarySearchTree<int>(56);
+            bst.AddNode(30);
+            bst.AddNode(70);
+
+            Console.WriteLine(bst.Root.Key);
+            Console.WriteLine(bst.Root.Left.Key);
+            Console.WriteLine(bst.Root.Right.Key);
         }
     }
 }
